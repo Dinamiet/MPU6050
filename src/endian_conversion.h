778f97378d1414edc96af9a65899261e94c5f1f7
Dinamiet <iceman.sm39@gmail.com>
2024-09-29 09:43:29 +0200

    Add endian conversion macros

---
+++ src/endian_conversion.h
@@ -0,0 +1,16 @@
+#ifndef _ENDIAN_CONVERSION_H_
+#define _ENDIAN_CONVERSION_H_
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define BIG_ENDIAN_16(x)    __builtin_bswap16(x)
+#define LITTLE_ENDIAN_16(x) (x)
+#define BIG_ENDIAN_32(x)    __builtin_bswap32(x)
+#define LITTLE_ENDIAN_32(x) (x)
+#else
+#define BIG_ENDIAN_16(x)    (x)
+#define LITTLE_ENDIAN_16(x) __builtin_bswap16(x)
+#define BIG_ENDIAN_32(x)    (x)
+#define LITTLE_ENDIAN_32(x) __builtin_bswap32(x)
+#endif
+
+#endif
