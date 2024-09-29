e46f4cf427a2774e4ccedc4053ed008424d35bc7
Dinamiet <iceman.sm39@gmail.com>
2024-09-29 09:48:15 +0200

    Add Gyro functions

--- include/mpu6050.h
+++ include/mpu6050.h
@@ -9,6 +9,20 @@ typedef void (*MPU_Complete)(bool success, uint8_t deviceID, size_t size);
 typedef size_t (*MPU_DataTransfer)(void* data, size_t size);
 typedef bool (*MPU_DataRequest)(size_t size, MPU_Complete completed);
 
+typedef struct _MPUOffset_
+{
+	int16_t X;
+	int16_t Y;
+	int16_t Z;
+} MPUOffset;
+
+typedef struct _MPURaw_
+{
+	int16_t X;
+	int16_t Y;
+	int16_t Z;
+} MPURaw;
+
 typedef struct _MPU_
 {
 	MPU_DataTransfer Read;
@@ -23,20 +37,19 @@ void MPU_Configure(MPU* mpu);
 void MPU_Enable(MPU* mpu);
 void MPU_Disable(MPU* mpu);
 
-void MPU_SetGyroOffset(MPU* mpu);
+void      MPU_SetGyroOffset(MPU* mpu, MPUOffset offset);
+void      MPU_RequestGyroOffset(MPU* mpu, MPU_Complete ready);
+MPUOffset MPU_GyroOffset(MPU* mpu);
+void      MPU_RequestRawGyro(MPU* mpu, MPU_Complete ready);
+MPURaw    MPU_RawGyro(MPU* mpu);
+void      MPU_CalibrateGyro(MPU* mpu, uint8_t minItt, uint8_t maxErr, void (*progress)(char));
+
 void MPU_SetAccelOffset();
-
-void MPU_RequestGyroOffset(MPU* mpu);
-void MPU_GyroOffset(MPU* mpu);
-
 void MPU_RequestAccelOffset();
 void MPU_AccelOffset();
-
-void MPU_RequestRawGyro();
-void MPU_RawGyro();
-
 void MPU_RequestRawAccel();
 void MPU_RawAccel();
+void MPU_CalibrateAccel();
 
 void MPU_RequestTemperature();
 void MPU_Temperature();
@@ -48,8 +61,5 @@ void MPU_RequestPacket();
 void MPU_Accel();
 void MPU_Gyro();
 void MPU_YawPitchRoll();
-
-void MPU_CalibrateAccel();
-void MPU_CalibrateGyro();
 
 #endif
---
+++ src/gyro.c
@@ -0,0 +1,58 @@
+#include "endian_conversion.h"
+#include "mpu6050.h"
+
+#include <string.h>
+
+#define GYRO_OFFSET_REG 0x13
+#define GYRO_RAW_REG    0x43
+
+void MPU_SetGyroOffset(MPU* mpu, MPUOffset offset)
+{
+	offset.X = BIG_ENDIAN_16(offset.X);
+	offset.Y = BIG_ENDIAN_16(offset.Y);
+	offset.Z = BIG_ENDIAN_16(offset.Z);
+
+	uint8_t data[1 + sizeof(offset)] = {GYRO_OFFSET_REG}; // Gyro offset register
+	memcpy(&data[1], &offset, sizeof(offset));
+	while (!mpu->Write(data, sizeof(data)));
+}
+
+void MPU_RequestGyroOffset(MPU* mpu, MPU_Complete ready)
+{
+	uint8_t reg = GYRO_OFFSET_REG;
+	while (!mpu->Write(&reg, sizeof(reg)));
+	while (!mpu->Request(sizeof(MPUOffset), ready));
+};
+
+MPUOffset MPU_GyroOffset(MPU* mpu)
+{
+	MPUOffset offset;
+	mpu->Read(&offset, sizeof(offset));
+
+	offset.X = BIG_ENDIAN_16(offset.X);
+	offset.Y = BIG_ENDIAN_16(offset.Y);
+	offset.Z = BIG_ENDIAN_16(offset.Z);
+
+	return offset;
+}
+
+void MPU_RequestRawGyro(MPU* mpu, MPU_Complete ready)
+{
+	uint8_t reg = GYRO_RAW_REG;
+	while (!mpu->Write(&reg, sizeof(reg)));
+	while (!mpu->Request(sizeof(MPURaw), ready));
+}
+
+MPURaw MPU_RawGyro(MPU* mpu)
+{
+	MPURaw raw;
+	mpu->Read(&raw, sizeof(raw));
+
+	raw.X = BIG_ENDIAN_16(raw.X);
+	raw.Y = BIG_ENDIAN_16(raw.Y);
+	raw.Z = BIG_ENDIAN_16(raw.Z);
+
+	return raw;
+}
+
+void MPU_CalibrateGyro(MPU* mpu, uint8_t minItt, uint8_t maxErr, void (*progress)(char)) { /** TODO: Add calibration routine */ }
