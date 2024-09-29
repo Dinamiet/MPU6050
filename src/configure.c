e2ae77cf4c12cfd9fb532ac6aa0f4c576e7c7d6a
Dinamiet <iceman.sm39@gmail.com>
2024-09-29 08:42:01 +0200

    Basic device configuration

---
+++ src/configure.c
@@ -0,0 +1,54 @@
+#include "mpu6050.h"
+
+#include <assert.h>
+
+static void setRegister(MPU* mpu, uint8_t reg, uint8_t value);
+
+static void setRegister(MPU* mpu, uint8_t reg, uint8_t value)
+{
+	uint8_t data[] = {reg, value};
+	while (!mpu->Write(data, sizeof(data), NULL));
+}
+
+void MPU_Init(MPU* mpu, MPU_DataTransfer read, MPU_DataTransfer write)
+{
+	assert(mpu != NULL);
+	assert(read != NULL);
+	assert(write != NULL);
+
+	mpu->Read  = read;
+	mpu->Write = write;
+
+	setRegister(mpu, 0x6B, 0x80); // Reset IMU
+}
+
+void MPU_Deinit(MPU* mpu) { /** TODO: Poweroff device */ }
+
+void MPU_Configure(MPU* mpu)
+{
+	setRegister(mpu, 0x6B, 0x01); // Setup clock source
+	setRegister(mpu, 0x23, 0x00); // No Fifo
+	setRegister(mpu, 0x38, 0x00); // No INT
+	setRegister(mpu, 0x37, 0x80); // INT active Low
+	setRegister(mpu, 0x1C, 0x00); // Accelerometer Scale
+	setRegister(mpu, 0x1B, 0x18); // Gyro Scale
+	setRegister(mpu, 0x19, 0x04); // Sample Rate
+	setRegister(mpu, 0x1A, 0x01); // Digital Filter
+
+	/** TODO: Write DMP Program */
+}
+
+void MPU_Enable(MPU* mpu)
+{
+	setRegister(mpu, 0x6A, 0xC4); // Enable DMP & Fifo, also reset Fifo
+
+	// Read interrupt status register to clear all interrupts
+	uint8_t reg = 0x3A;
+	while (!mpu->Write(&reg, sizeof(reg), NULL));
+	while (!mpu->Read(NULL, 1, NULL));
+}
+
+void MPU_Disable(MPU* mpu)
+{
+	setRegister(mpu, 0x6A, 0x04); // Disable DMP & Fifo, also reset Fifo
+}
